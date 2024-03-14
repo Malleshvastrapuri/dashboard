1f4bcdfa40127c76c267386249d681632c8a8c0b
admin <admin@admins-MacBook-Pro.local>
2024-03-14 15:39:57 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.js'
+import { BrowserRouter } from 'react-router-dom'
+import { CartProvider } from './stores/context/CartContext.js'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+<BrowserRouter>
+  <React.StrictMode>
+    <CartProvider>
+    <App />
+    </CartProvider>
+  </React.StrictMode>,
+</BrowserRouter>
+)
